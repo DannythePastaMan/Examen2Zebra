2e6c3df6a1f0e1c750afa492254d41506ea4aedf
DannythePastaMan <dgomez0998@gmail.com>
2017-09-18 15:58:28 +0200

    Add files via upload

---
+++ NodoBinario.cpp
@@ -0,0 +1,14 @@
+
+#include "NodoBinario.h"
+
+NodoBinario::NodoBinario(int valor)
+{
+    this->valor = valor;
+    this->derecho = NULL;
+    this->izquierdo = NULL;
+}
+
+NodoBinario::~NodoBinario()
+{
+    //dtor
+}
